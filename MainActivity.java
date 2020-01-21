faae9a59db0599049c25b1a37aa7ee7bb75748ec
shenc1988 <sc1988sc1988@163.com>
2020-01-21 22:24:51 +0800

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,9 +1,10 @@
-package com.example.myfirstapplication;
+package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -253,6 +254,4 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, SceneActivity.class);
         startActivity(intent);
     }
-
 }
-

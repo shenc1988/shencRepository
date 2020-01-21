7471af3a25128e28e178954dbb0aa1ba4533dd05
shenc1988 <59814360+shenc1988@users.noreply.github.com>
2020-01-21 18:12:31 +0800

    Create Land.java

---
+++ Land.java
@@ -0,0 +1,27 @@
+package com.example.myfirstapplication;
+
+public class Land {
+    // 坐标
+    private int x, y;
+
+    public Land(int x, int y) {
+        this.setX(x);
+        this.setY(y);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
